6999a70738dd43b6b56522fbf76fe81f29f559b0
Diego Lorden <djlorden@gmail.com>
2013-06-14 16:23:23 -0700

    Question 1.8

---
+++ 01.Arrays_and_Strings/08/rotation.py
@@ -0,0 +1,12 @@
+def is_substring(haystack, needle):
+    return needle in haystack
+
+def is_rotation(str1, str2):
+    if len(str1) != len(str2):
+        return False
+    str1 += str1
+    return is_substring(str1, str2)
+
+str1 = raw_input('String 1: ')
+str2 = raw_input('String 2: ')
+print is_rotation(str1, str2)
